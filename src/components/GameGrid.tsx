95bcba7c4618c1f46ce7ed5c012598a643869f73
Daniel Lachica <daniel.lachica82@gmail.com>
2026-06-05 12:00:43 +0800

    Fetch games from rawg API

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Text } from "@chakra-ui/react";
+import apiClient from "@/services/api-client";
+
+// https://api.rawg.io/docs/#operation/games_list
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  // Send fetch request to backend
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return (
+    <>
+      {error && <Text color="red">{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
