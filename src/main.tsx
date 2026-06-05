e3ef2b8f24ce4296f84e7a433d47c18e7bee6479
Daniel Lachica <daniel.lachica82@gmail.com>
2026-06-05 10:41:17 +0800

    Set dark mode as default

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "./components/ui/provider";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/components/ui/color-mode";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <App />
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
