6f9aefa20d4c634ec76e9e6078af1003cbd4618a
Daniel Lachica <daniel.lachica82@gmail.com>
2026-06-10 06:01:48 +0800

    Handle null metacritic score

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -7,9 +7,16 @@ interface Props {
 const CriticScore = ({ score = 0 }: Props) => {
   const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
-    <Badge fontSize="14px" paddingX={2} borderRadius="4px" colorPalette={color}>
-      {score}
-    </Badge>
+    score && (
+      <Badge
+        fontSize="14px"
+        paddingX={2}
+        borderRadius="4px"
+        colorPalette={color}
+      >
+        {score}
+      </Badge>
+    )
   );
 };
 
